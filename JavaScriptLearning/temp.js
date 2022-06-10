dcf599cfa2b9db85c64c762dcd0b3364f35f3afe
Gausshj <kylin_0@qq.com>
2022-06-10 10:42:18 +0800

    Delete

--- JavaScriptLearning/temp.js
+++
@@ -1,7 +0,0 @@
-
-var colors1 = ['red', 'green', 'blue'];
-var colors2 = ['yellow', 'black'];
-console.log(colors1.concat(colors2));
-console.log(colors2.concat(colors1));
-console.log(colors2.concat('brown'));
-console.log(colors2)//注意：concat返回一个新数组，原数组没改变
